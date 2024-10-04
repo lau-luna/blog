9379feec995b9ee22005f3e3c79366ec632d255b
lautaro-luna <128639299+lau-luna@users.noreply.github.com>
2024-10-03 21:00:34 -0300

    36. Sacar categorías

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,11 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function pruebas(Request $request) {
-        return "Acción de pruebas de CategoryController";
+    public function index(){
+        $categories = Category::all();
+
+        return response()->json([
+            'code'       => 200,
+            'status'     => 'success',
+            'categories' => $categories
+        ]);
+    }
+
+    public function show($id) {
+        $category = Category::find($id);
+
+        if(is_object($category)) {
+            $data = [
+                'code'       => 200,
+                'status'     => 'success',
+                'category' => $category
+            ];
+        }else {
+            $data = [
+                'code'       => 404,
+                'status'     => 'error',
+                'message' => "La categoría no existe"
+            ];
+        }
+
+        return response()->json($data, $data['code']);
     }
 }
