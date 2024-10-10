ce58893b456da026860fdc1870d0e3808a2ce9e8
lautaro-luna <128639299+lau-luna@users.noreply.github.com>
2024-10-10 08:59:58 -0300

    48. Configurar el CORS

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Author");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
